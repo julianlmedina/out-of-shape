f995d1ade7e734ef3851348c986981b818a2f220
Julian Medina <cybernerd94@gmail.com>
2015-11-12 20:52:32 -0800

    Update Mouse.js

--- public/js/Mouse.js
+++ public/js/Mouse.js
@@ -1,25 +1,32 @@
+/* onMouseMove Listener **/
 window.addEventListener( 'mousemove', onMouseMove, false );
-				
+
+/* onMouseDown Listener **/				
 window.addEventListener( 'mousedown', onMouseDown, false );
 
+/* addEvent Listener **/
 window.addEventListener( 'mouseup', onMouseUp, false );
 
 //http://threejs.org/docs/#Reference/Core/Raycaster
 
+// Create new Mouse instance
 var mouse = new Mouse();
 
+/* Prevent default action of onMouseDown. **/
 function onMouseDown (event)
 {
 	event.preventDefault();
 	//mouse.UpdateClick
 }
 
+/* Prevent default action of onMouseUp. **/
 function onMouseUp (event)
 {
 	event.preventDefault();
 	//mouse.UpdateClick
 }
 
+/* Prevent default action of onMouseMove. **/
 function onMouseMove( event ) {
 
 	// calculate mouse position in normalized device coordinates
@@ -29,12 +36,16 @@ function onMouseMove( event ) {
 
 }
 
-
+/* Mouse function **/
 function Mouse()
 {
+	/* Create new raycaster instance. **/
 	var raycaster = new THREE.Raycaster();
+	
+	/* Create new position instance. **/
 	var position = new THREE.Vector2();
 	
+	/* Set x and y positions. **/
 	this.SetPosition = function(givenx, giveny)
 	{
 		position.x = givenx;
